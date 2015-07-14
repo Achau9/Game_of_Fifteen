56691526e2bd793ecf7e9943cf6ee5473258291a
Alston Chau <Achau9@Alstons-MacBook-Pro.local>
2015-07-14 11:23:24 -0400

    First commit

---
+++ Game of Fifteen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Game of Fifteen
+//
+//  Created by Alston Chau on 7/14/15.
+//  Copyright (c) 2015 Alston Chau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
